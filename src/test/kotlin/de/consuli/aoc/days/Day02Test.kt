f601ac2c244d27f179b41e669d565c926515f9be
ulrichschulte <ulrich.schulte@codecentric.de>
2022-12-03 15:28:18 +0100

    Refactored test setup

--- src/test/kotlin/de/consuli/aoc/days/Day02Test.kt
+++ src/test/kotlin/de/consuli/aoc/days/Day02Test.kt
@@ -1,19 +1,10 @@
 package de.consuli.aoc.days
 
-import org.junit.jupiter.api.Test
-
-class Day02Test {
-    private var day02 = Day02()
-
-    @Test
-    fun partOne() {
-        kotlin.test.assertEquals(15, day02.partOne(true))
-        kotlin.test.assertEquals(10941, day02.partOne(false))
-    }
-
-    @Test
-    fun partTwo() {
-        kotlin.test.assertEquals(12, day02.partTwo(true))
-        kotlin.test.assertEquals(13071, day02.partTwo(false))
-    }
-}
+class Day02Test: DayTest(
+    Day02(), PuzzleOutput(
+        partOneExpectedSampleOutput = 15,
+        partOneExpectedOutput = 10941,
+        partTwoExpectedSampleOutput = 12,
+        partTwoExpectedOutput = 13071
+    )
+)
---
+++ src/test/kotlin/de/consuli/aoc/days/DayTest.kt
@@ -0,0 +1,71 @@
+package de.consuli.aoc.days
+
+import de.consuli.aoc.Day
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.util.function.Supplier
+import kotlin.system.measureTimeMillis
+import kotlin.test.assertEquals
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class DayTest(private val day: Day, private val expectedOutput: PuzzleOutput) {
+
+    @BeforeAll
+    internal fun printTestOutputHeader() {
+        println("===== Testing solution for day ${day.dayNumber} =====")
+    }
+
+    @Test
+    fun partOne() {
+        this.expectedOutput.partOneExpectedOutput?.let {
+            executePart(
+                1,
+                it,
+                false,
+                outputSupplier = { day.partOne(false) })
+        }
+        this.expectedOutput.partOneExpectedSampleOutput?.let {
+            executePart(
+                1,
+                it,
+                true,
+                outputSupplier = { day.partOne(true) })
+        }
+    }
+
+    @Test
+    fun partTwo() {
+        this.expectedOutput.partTwoExpectedOutput?.let {
+            executePart(
+                2,
+                it,
+                false,
+                outputSupplier = { day.partTwo(false) })
+        }
+        this.expectedOutput.partTwoExpectedSampleOutput?.let {
+            executePart(
+                2,
+                it,
+                true,
+                outputSupplier = { day.partTwo(true) })
+        }
+    }
+
+    private fun executePart(part: Int, expectedOutput: Any, isSample: Boolean, outputSupplier: Supplier<Any>) {
+        val computedOutput: Any
+        val timeInMillis = measureTimeMillis {
+            computedOutput = outputSupplier.get()
+        }
+        assertEquals(expectedOutput, computedOutput)
+        println("Day ${day.dayNumber} part $part ${if (isSample) " sample " else " "}took $timeInMillis ms")
+    }
+
+}
+
+data class PuzzleOutput(
+    val partOneExpectedSampleOutput: Any?,
+    val partOneExpectedOutput: Any?,
+    val partTwoExpectedSampleOutput: Any?,
+    val partTwoExpectedOutput: Any?
+)
