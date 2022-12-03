8b226b43f70f87c3d67371a213c006450ddfe8af
ulrichschulte <ulrich.schulte@codecentric.de>
2022-12-03 16:33:46 +0100

    Added solution for day 3.

---
+++ src/main/kotlin/de/consuli/aoc/days/Day03.kt
@@ -0,0 +1,30 @@
+package de.consuli.aoc.days
+
+import de.consuli.aoc.Day
+
+class Day03 : Day(3) {
+    override fun partOne(testInput: Boolean): Any {
+        return getInput(testInput).sumOf {
+            getPriorityForItemInBothCompartments(it)
+        }
+    }
+
+    override fun partTwo(testInput: Boolean): Any {
+        return getInput(testInput).windowed(3, 3).sumOf { rucksacks ->
+            val commonItem = rucksacks[0].find { it in rucksacks[1] && it in rucksacks[2] } ?: ' '
+            getPriority(commonItem)
+        }
+    }
+
+    private fun getPriorityForItemInBothCompartments(line: String): Int {
+        val itemsInCompartment = line.length / 2
+        val itemsFirstCompartment = line.substring(0, itemsInCompartment)
+        val itemsSecondCompartment = line.substring(itemsInCompartment, line.length)
+        return getPriority(itemsFirstCompartment.find { it in itemsSecondCompartment } ?: ' ')
+    }
+
+    private fun getPriority(item: Char): Int {
+        return if (item in 'a'..'z') item - 'a' + 1 else item - 'A' + 27
+    }
+
+}
