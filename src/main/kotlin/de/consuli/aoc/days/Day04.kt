df09fb70149ad55bc77a33f87329b413a0ca3a1b
ulrichschulte <ulrich.schulte@codecentric.de>
2022-12-04 14:00:55 +0100

    Added solution for day 4.

---
+++ src/main/kotlin/de/consuli/aoc/days/Day04.kt
@@ -0,0 +1,28 @@
+package de.consuli.aoc.days
+
+import de.consuli.aoc.Day
+
+class Day04 : Day(4) {
+    override fun partOne(testInput: Boolean): Any {
+        return getInput(testInput).count { sectionAssignment ->
+            val sections = getSections(sectionAssignment)
+            sections.first.first in sections.second && sections.first.last in sections.second
+                    || sections.second.first in sections.first && sections.second.last in sections.first
+        }
+    }
+
+    override fun partTwo(testInput: Boolean): Any {
+        return getInput(testInput).count { sectionAssignment ->
+            val sections = getSections(sectionAssignment)
+            sections.first.first in sections.second || sections.first.last in sections.second
+                    || sections.second.first in sections.first || sections.second.last in sections.first
+        }
+    }
+
+    private fun getSections(sectionAssignment: String): Pair<IntRange, IntRange> {
+        val sectionAssignmentPairs = sectionAssignment.split(",")
+        val (startSection1, endSection1) = sectionAssignmentPairs[0].split("-").map { it.toInt() }
+        val (startSection2, endSection2) = sectionAssignmentPairs[1].split("-").map { it.toInt() }
+        return Pair(startSection1..endSection1, startSection2..endSection2)
+    }
+}
---
+++ src/test/kotlin/de/consuli/aoc/days/Day04Test.kt
@@ -0,0 +1,11 @@
+package de.consuli.aoc.days
+
+class Day04Test : DayTest(
+    Day04(), PuzzleOutput(
+        partOneExpectedSampleOutput = 2,
+        partOneExpectedOutput = 602,
+        partTwoExpectedSampleOutput = 4,
+        partTwoExpectedOutput = 891
+    )
+)
+
