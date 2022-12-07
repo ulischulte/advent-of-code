916f0c9b01588e7932d1ccd13a12f681f774bf3f
ulrichschulte <ulrich.schulte@codecentric.de>
2022-12-07 08:21:05 +0100

    Added solution for day 6.

---
+++ src/main/kotlin/de/consuli/aoc/days/Day06.kt
@@ -0,0 +1,17 @@
+package de.consuli.aoc.days
+
+import de.consuli.aoc.Day
+
+class Day06 : Day(6) {
+    override fun partOne(testInput: Boolean): Any {
+        return findFirstMarker(getInput(testInput).first(), 4)
+    }
+
+    override fun partTwo(testInput: Boolean): Any {
+        return findFirstMarker(getInput(testInput).first(), 14)
+    }
+
+    private fun findFirstMarker(input: String, markerSize: Int): Int {
+        return input.windowed(markerSize).indexOfFirst { it.toSet().size == markerSize } + markerSize
+    }
+}
---
+++ src/test/kotlin/de/consuli/aoc/days/Day06Test.kt
@@ -0,0 +1,10 @@
+package de.consuli.aoc.days
+
+class Day06Test : DayTest(
+    Day06(), PuzzleOutput(
+        partOneExpectedSampleOutput = 10,
+        partOneExpectedOutput = 1566,
+        partTwoExpectedSampleOutput = 29,
+        partTwoExpectedOutput = 2265
+    )
+)
