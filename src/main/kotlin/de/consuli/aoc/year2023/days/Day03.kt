30f08f5533dc3a5d4123ece89212d015beaeda71
ulrichschulte <ulrich.schulte@codecentric.de>
2023-12-04 09:39:35 +0100

    Refactored day 3

--- src/main/kotlin/de/consuli/aoc/year2023/days/Day03.kt
+++ src/main/kotlin/de/consuli/aoc/year2023/days/Day03.kt
@@ -4,43 +4,33 @@ import de.consuli.aoc.common.Day
 
 class Day03 : Day(3, 2023) {
 
-    override fun partOne(testInput: Boolean): Int = calculateSumOfParts(toCharArrayList(getInput(testInput)))
+    data class Point(val y: Int, val x: Int)
 
-    override fun partTwo(testInput: Boolean): Int = calculateGearRatio(getInput(testInput))
+    override fun partOne(testInput: Boolean): Int =
+        calculateSumOfMetrics(toCharArrayList(getInput(testInput)), this::partOnePredicate, this::partOneSelector)
 
-    private fun calculateSumOfParts(input: List<CharArray>): Int {
-        return sumValidPartNumbers(input, { !it.isDigit() && it != '.' }) { row: Int, col: Int ->
-            getAdjacentNonZeroNumbers(input, row, col).sum()
-        }
-    }
+    override fun partTwo(testInput: Boolean): Int =
+        calculateSumOfMetrics(toCharArrayList(getInput(testInput)), this::partTwoPredicate, this::partTwoSelector)
 
-    private fun calculateGearRatio(input: List<String>): Int {
-        data class YX(val y: Int, val x: Int)
-
-        val starList = mutableListOf<YX>()
-        val charArrayList = toCharArrayList(input)
-        charArrayList.forEachIndexed { y, chars ->
-            chars.forEachIndexed { x, char ->
-                if (char == '*') starList.add(YX(y, x))
-            }
-        }
-        val sumOfGearRatios = starList.sumOf { star ->
-            val numbersNextToStars = getAdjacentNonZeroNumbers(charArrayList, star.y, star.x)
-            if (numbersNextToStars.size == 2) numbersNextToStars[0] * numbersNextToStars[1]
-            else 0
-        }
-        return sumOfGearRatios
-    }
-
-    private fun sumValidPartNumbers(
-        matrix: List<CharArray>,
-        predicate: (char: Char) -> Boolean,
-        selector: (row: Int, col: Int) -> Int,
+    private fun calculateSumOfMetrics(
+        input: List<CharArray>,
+        predicate: (Char) -> Boolean,
+        selector: (Point, List<CharArray>) -> Int
     ): Int {
-        return matrix.indices.flatMap { row ->
-            matrix[row].indices.map { col -> Pair(row, col) }
-        }
-            .filter { (row, col) -> predicate(matrix[row][col]) }.sumOf { (row, col) -> selector(row, col) }
+        return input.indices
+            .flatMap { row -> input[row].indices.map { col -> Point(row, col) } }
+            .filter { (row, col) -> predicate(input[row][col]) }.sumOf { selector(it, input) }
+    }
+
+    private fun partOnePredicate(c: Char) = !c.isDigit() && c != '.'
+    private fun partOneSelector(point: Point, matrix: List<CharArray>) =
+        getAdjacentNonZeroNumbers(matrix, point.y, point.x).sum()
+
+    private fun partTwoPredicate(c: Char) = c == '*'
+    private fun partTwoSelector(point: Point, matrix: List<CharArray>): Int {
+        val numbersNextToStars = getAdjacentNonZeroNumbers(matrix, point.y, point.x)
+        return if (numbersNextToStars.size == 2) numbersNextToStars[0] * numbersNextToStars[1]
+        else 0
     }
 
     private fun getAdjacentNonZeroNumbers(matrix: List<CharArray>, row: Int, col: Int): List<Int> {
@@ -56,15 +46,16 @@ class Day03 : Day(3, 2023) {
             -1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1
         ).map { Pair(row + it.first, column + it.second) }
 
-    private fun extractNumberAt(matrix: CharArray, index: Int): Int {
-        if (!matrix[index].isDigit()) return 0
+    private fun extractNumberAt(row: CharArray, index: Int): Int {
+        if (!row[index].isDigit()) return 0
 
-        val numberStart = (index downTo 0).find { !matrix[it].isDigit() }?.plus(1) ?: 0
-        val numberEnd = (index..matrix.lastIndex).find { !matrix[it].isDigit() }?.minus(1) ?: matrix.lastIndex
+        val numberStart = (index downTo 0).find { !row[it].isDigit() }?.plus(1) ?: 0
+        val numberEnd = (index..row.lastIndex).find { !row[it].isDigit() }?.minus(1) ?: row.lastIndex
 
-        val number = matrix.slice(numberStart..numberEnd).joinToString(separator = "").toInt()
+        val number = row.slice(numberStart..numberEnd).joinToString(separator = "").toInt()
 
-        for (i in numberStart..numberEnd) matrix[i] = '.'
+        // mask processed number chars with '.'
+        for (i in numberStart..numberEnd) row[i] = '.'
 
         return number
     }
